d4d474e09887cef0e0540157c9161d14ff594cc7
Jacques Michiels <jacquesmichiels@gmail.com>
2018-02-15 15:55:09 -0500

    fixed source filter

--- src/main.c
+++ src/main.c
@@ -4,12 +4,13 @@ int main(void) {
     /* Initializes MCU, drivers and middleware */
     atmel_start_init();
 
-    while (1) {
-        delay_ms(1000);
-        // Just blink to make sure it works.
-        gpio_toggle_pin_level(LED_BUILTIN);
-    }
-
     /* USB 'Echo' example */
     // cdcd_acm_example();
+
+    while (1) {
+        delay_ms(500);
+
+        // Just blink the built-in led.
+        gpio_toggle_pin_level(LED_BUILTIN);
+    }
 }
